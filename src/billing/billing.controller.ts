f821629dacc44cde9232447e9f53e035baf53e52
Narjes Taghlet <99217840+NarjesTaghlet@users.noreply.github.com>
2025-07-14 14:24:09 +0100

    update

--- src/billing/billing.controller.ts
+++ src/billing/billing.controller.ts
@@ -160,7 +160,7 @@ async checkAlert(@Request () req , @Body() body : any ): Promise<{ alert: boolea
   }
 }
   */
-import { Buffer } from 'buffer';
+import { Buffer } from 'buffer/';  // Ajoutez un slash à la fin
 
 import {
   Controller,
