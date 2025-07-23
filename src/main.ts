d8a9b946f51d7939a2d59a6ed251762c13fbdf20
Narjes Taghlet <99217840+NarjesTaghlet@users.noreply.github.com>
2025-07-23 10:02:09 +0100

    udate

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,46 @@ import * as express from 'express';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS to allow requests from Angular frontend (or other clients)
-  app.enableCors({
-    origin: 'http://localhost:4200',  // URL of your Angular app (adjust as needed)
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],  // Allowed methods
-    allowedHeaders: ['Content-Type', 'Authorization'],  // Allowed headers
+    app.enableCors({
+    origin: ['http://localhost:4200','https://d398rqqt4ze3my.cloudfront.net',    'https://d1no5jk0cuzn91.cloudfront.net','https://d2k1rrgcfjq38f.cloudfront.net'
+] ,// ✅ Autoriser uniquement le frontend Angular
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true, // ✅ Si besoin d'authentification (JWT, Cookies)
   });
+
+ //await app.listen(3004);
+  // Express CORS Middleware
+const corsMiddleware = (req, res, next) => {
+  const allowedOrigins = [
+    'https://dpfzuq7w5fb82.cloudfront.net',
+        'https://d1no5jk0cuzn91.cloudfront.net',
+        'https://d2k1rrgcfjq38f.cloudfront.net',
+
+    'https://*.cloudfront.net',
+    'http://localhost:3030' // For local development
+  ];
+  
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Amz-Date, X-Api-Key, X-Amz-Security-Token');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  
+  next();
+};
+
+// Use the middleware in all services
+app.use(corsMiddleware);
+ 
 
  app.use(
     '/billing/webhook',
